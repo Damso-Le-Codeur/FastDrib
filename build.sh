2c7f3780db42f33145f71897a1afc1e75083042f
tanou-web <tanouahmadou043@gmail.com>
2025-12-17 15:03:16 +0000

    Configuration pour Render

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Installer les dépendances
+pip install -r requirements.txt
+
+# Collecter les fichiers statiques
+python manage.py collectstatic --no-input
+
+# Appliquer les migrations
+python manage.py migrate
